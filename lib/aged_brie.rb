b05b7f278cf391a0b44618b00dd72a554e03273a
Dan Pelensky <dan@pelensky.com>
2017-01-25 11:23:25 +0000

    aged brie class created, not yet refactored out

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -1,9 +1,38 @@
 class AgedBrie < Item
 
-    def initialize(name = "Aged Brie", sell_in, quality)
-      @name = name
-      @sell_in = sell_in
-      @quality = quality
-    end
-
+  def initialize(name = "Aged Brie", sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
   end
+
+  def update_quality
+    @quality += 1 if over_ten_days
+    @quality += 2 if six_to_ten_days
+    @quality += 3 if zero_to_five_days
+    set_quality_to_50 if quality_over_50
+  end
+
+  private
+
+  def over_ten_days
+    @sell_in > 10
+  end
+
+  def six_to_ten_days
+    @sell_in > 5 && @sell_in < 11
+  end
+
+  def zero_to_five_days
+    @sell_in >= 0 && @sell_in < 6
+  end
+
+  def set_quality_to_50
+    @quality = 50
+  end
+
+  def quality_over_50
+    @quality > 50
+  end
+
+end
--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -2,7 +2,6 @@ describe AgedBrie do
   subject(:aged_brie){ described_class.new("Aged Brie", 5, 5)}
 
   context "#initialize" do
-
     it "initializes with a name" do
       expect(aged_brie.name).to eq "Aged Brie"
     end
@@ -12,8 +11,38 @@ describe AgedBrie do
     it "initializes with a quality number" do
       expect(aged_brie.quality).to be_a_kind_of(Integer)
     end
-
   end
 
+
+  context "#update_quality" do
+
+    it "should never increase the quality over 50" do
+      aged_brie.quality = 49
+      aged_brie.update_quality
+      expect(aged_brie.quality).to eq 50
+    end
+
+    it "if there are over 10 days, quality increases by one" do
+      aged_brie.sell_in = 15
+      aged_brie.quality = 10
+      aged_brie.update_quality
+      expect(aged_brie.quality).to eq 11
+    end
+
+    it "if there are between 6 and 10 days, quality increases by two" do
+      aged_brie.sell_in = 8
+      aged_brie.quality = 10
+      aged_brie.update_quality
+      expect(aged_brie.quality).to eq 12
+    end
+
+    it "if there are between 0 and 5 days, quality increases by three" do
+      aged_brie.sell_in = 0
+      aged_brie.quality = 10
+      aged_brie.update_quality
+      expect(aged_brie.quality).to eq 13
+    end
+
+  end
 
 end
